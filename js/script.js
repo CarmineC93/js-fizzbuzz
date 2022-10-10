6a2eb7514eda0b36aa830c3b7d462976a548906e
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-10 15:49:09 +0200

    Details

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-/* **Consegna:**
+/*  **Consegna:**
  Scrivi un programma che stampi in console i numeri da 1 a 100,
  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
- Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. 
+ 
+    **BONUS 1:**
+Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento 
+html con il numero o la stringa corretta da mostrare.
+
+ 
+    **BONUS 2:**
+ Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+ Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.*/
 
  //Apriamo un loop che vada da 1 a 100 e che proceda con incremento di uno
 for (let i = 1; i <= 100; i++){
@@ -22,7 +31,7 @@ for (let i = 1; i <= 100; i++){
     //Creiamo attraverso .createElement un elemento div, salvandolo in una variabile
     const col = document.createElement("div");
     //Assegniamo a questo div una classe
-    col.classList.add("col", "text-center", "fw-bold", "fs-2");
+    col.classList.add("col", "col-md-1", "text-center", "fw-bold", "fs-5", "g-3", "ms-3");
     //In questo elemento inseriamo il valore di i, salvato nella variabile number
     col.innerHTML = number;
     //verifichiamo che si siano creati 100 div con all'interno valori crescenti da 1 a 100 
@@ -30,21 +39,11 @@ for (let i = 1; i <= 100; i++){
     //Spostiamo questo elemento nel DOM attraverso .append()
     document.querySelector(".row").append(col);
 
+    //Creiamo 4 classi in CSS con differenti background per ciascuno dei 4 valori ottenibili dal IF (numero, stringa Fizz, stringaBuzz, stringaFizzBuzz)
+    //Per evitare di dover creare un secondo IF il nome delle 3 classi stringa sarà lo stesso dei valori ottenibili dal primo IF
+    //Assegneremo quindi al div una nuova classe attraverso .classList che sarà uguale al valore attuale della costante number.
     col.classList.add(number);
 
 }
 
-/* **BONUS 1:**
-Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento 
-html con il numero o la stringa corretta da mostrare.
-
- 
-
-  **BONUS 2:**
- Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
- Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.*/
-
- //Creiamo 4 classi in CSS con differenti background per ciascuno dei 4 valori ottenibili dal IF (numero, stringa Fizz, stringaBuzz, stringaFizzBuzz)
- //Per evitare di dover creare un secondo IF il nome delle 3 classi stringa sarà lo stesso dei valori ottenibili dal primo IF
- //Assegneremo quindi al div una nuova classe attraverso .classList che sarà uguale al valore attuale della costante number.
 
