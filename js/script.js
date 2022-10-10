cef5e901accef3a66b4f4a3049c95a967376c366
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-10 13:56:30 +0200

    Textual code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* **Consegna:**
+ Scrivi un programma che stampi in console i numeri da 1 a 100,
+ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+ //Apriamo un loop che vada da 1 a 100 e che proceda con incremento di uno
+
+ //Per ogni numero ottenuto SE il numero è divisibile per 3 e per 5 loggherà FizzBuzz
+
+ //Se divisibile per 3 loggherà Fizz
+
+ //Se divisibile per 5 loggherà Buzz
+
+ //Altrimenti loggherà il semplice numero 
