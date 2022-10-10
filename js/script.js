18f665a570136dd06230bfa4e0f23d6be5e50892
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-10 14:06:47 +0200

    FizzBuzz Works in console

--- js/script.js
+++ js/script.js
@@ -4,11 +4,22 @@
  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
  //Apriamo un loop che vada da 1 a 100 e che proceda con incremento di uno
+for (let i = 1; i <= 100; i++){
+    let number = i;
+    if(i % 3 == 0 && i % 5 == 0){
+        number = "FizzBuzz";
+    } else if (i % 3 == 0){
+        number = "Fizz";
+    } else if (i % 5 == 0) {
+        number = "Buzz";
+    }
 
- //Per ogni numero ottenuto SE il numero è divisibile per 3 e per 5 loggherà FizzBuzz
+    console.log(number);
+}
+    //Per ogni numero ottenuto SE il numero è divisibile per 3 E per 5 loggherà FizzBuzz
 
- //Se divisibile per 3 loggherà Fizz
+    //Se divisibile per 3 loggherà Fizz
 
- //Se divisibile per 5 loggherà Buzz
+    //Se divisibile per 5 loggherà Buzz
 
- //Altrimenti loggherà il semplice numero 
+    //Altrimenti loggherà il semplice numero 
